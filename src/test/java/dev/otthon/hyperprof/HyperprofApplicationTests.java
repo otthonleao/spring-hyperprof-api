634d3830ba27e0214300fe3ce48963a6a007b9b2
otthonleao <otthonleao@hotmail.com>
2024-04-23 20:26:18 -0400

    build: Base Project (Initial Commit)

---
+++ src/test/java/dev/otthon/hyperprof/HyperprofApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.hyperprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperprofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
