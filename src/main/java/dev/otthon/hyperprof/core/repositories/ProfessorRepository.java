4e3bc134796038f90f866ead6fb5f4f6cf0f2976
otthonleao <otthonleao@hotmail.com>
2024-04-25 14:25:06 -0400

    chore: add `findByDescricaoContaining` and eliminating wrong annotation

--- src/main/java/dev/otthon/hyperprof/core/repositories/ProfessorRepository.java
+++ src/main/java/dev/otthon/hyperprof/core/repositories/ProfessorRepository.java
@@ -2,8 +2,11 @@ package dev.otthon.hyperprof.core.repositories;
 
 import dev.otthon.hyperprof.core.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    List<Professor> findByDescricaoContaining(String descricao);
+
 }
