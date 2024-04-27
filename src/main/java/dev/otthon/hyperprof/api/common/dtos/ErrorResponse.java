0395cee19ef69465b912fca8f8601076502cfe05
otthonleao <otthonleao@hotmail.com>
2024-04-27 18:17:21 -0400

    refactor: change annotation `@builder` to `@SuperBuilder`

--- src/main/java/dev/otthon/hyperprof/api/common/dtos/ErrorResponse.java
+++ src/main/java/dev/otthon/hyperprof/api/common/dtos/ErrorResponse.java
@@ -3,14 +3,14 @@ package dev.otthon.hyperprof.api.common.dtos;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
