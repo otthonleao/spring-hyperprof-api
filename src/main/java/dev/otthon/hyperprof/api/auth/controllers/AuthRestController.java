7ebf1494191bb96cf531cb554db8efe1c3f6c7ff
otthonleao <otthonleao@hotmail.com>
2024-04-29 19:00:07 -0400

    feat: add Login Controllers (POST) for case PR02

---
+++ src/main/java/dev/otthon/hyperprof/api/auth/controllers/AuthRestController.java
@@ -0,0 +1,23 @@
+package dev.otthon.hyperprof.api.auth.controllers;
+
+import dev.otthon.hyperprof.api.auth.dtos.LoginRequest;
+import dev.otthon.hyperprof.api.auth.dtos.LoginResponse;
+import dev.otthon.hyperprof.api.auth.services.AuthService;
+import dev.otthon.hyperprof.api.common.routes.ApiRoutes;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthRestController {
+
+    private final AuthService authService;
+
+    @PostMapping(ApiRoutes.LOGIN)
+    public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+}
