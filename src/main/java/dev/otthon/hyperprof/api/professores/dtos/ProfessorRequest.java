802f055cac85513c5aaf6c0733721e1ed1563233
otthonleao <otthonleao@hotmail.com>
2024-04-28 22:52:31 -0400

    chore: add DTO `ProfessorRequest` for case PR01 - Cadastro de Professor

---
+++ src/main/java/dev/otthon/hyperprof/api/professores/dtos/ProfessorRequest.java
@@ -0,0 +1,56 @@
+package dev.otthon.hyperprof.api.professores.dtos;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ProfessorRequest {
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    private String nome;
+
+    @Email
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 255)
+    private String email;
+
+    @NotNull
+    @Min(18)
+    @Max(100)
+    private int idade;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 500)
+    private String descricao;
+
+    @NotNull
+    @Min(10)
+    @Max(500)
+    private BigDecimal valorHora;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 6, max = 255)
+    private String password;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 6, max = 255)
+    private String passwordConfirmation;
+
+}
